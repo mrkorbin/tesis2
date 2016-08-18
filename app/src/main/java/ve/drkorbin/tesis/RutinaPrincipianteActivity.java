1e3263e67ee9012302a3ee62877e3dfed7a3f142
mrkorbin <figuerad@hotmail.com>
2016-08-17 23:49:21 -0430

    Creacion de pantallas de contacto rutina avanzada y principiante
    Se crean las clases que contendra la informacion de las guias con rutina
    para entrenamientos avanzados y principiantes y se edita la ventana de
    contacto, falta la fiuncionalidad del enviar de contactanos

--- app/src/main/java/ve/drkorbin/tesis/RutinaPrincipianteActivity.java
+++ app/src/main/java/ve/drkorbin/tesis/RutinaPrincipianteActivity.java
@@ -5,18 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MusculoIndivActivity extends AppCompatActivity {
+public class RutinaPrincipianteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_musculo_indiv);
-    }
-
-    public void openMuscSeleccActivity(View view) {
-        Intent toMuscSeleccActivity = new Intent(getApplicationContext(), MuscSeleccActivity.class);
-        startActivity(toMuscSeleccActivity);
-
+        setContentView(R.layout.activity_rutina_principiante);
     }
 
     public void openPrincipalActivity(View view) {
@@ -24,5 +18,4 @@ public class MusculoIndivActivity extends AppCompatActivity {
         startActivity(toPrincipaActivity);
 
     }
-
 }

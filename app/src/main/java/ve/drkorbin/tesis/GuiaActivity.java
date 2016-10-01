fbc201f1805febf80e7a1bb0b8c41dd6e0bf3a4c
mrkorbin <figuerad@hotmail.com>
2016-09-30 23:24:59 -0430

    Adicion de imagenes de background
    Se agregaron imagenes y titulos a llos layouts

--- app/src/main/java/ve/drkorbin/tesis/GuiaActivity.java
+++ app/src/main/java/ve/drkorbin/tesis/GuiaActivity.java
@@ -1,7 +1,9 @@
 package ve.drkorbin.tesis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GuiaActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class GuiaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guia);
     }
+
+    public void openPrincipalActivity(View view) {
+        Intent toPrincipaActivity = new Intent(getApplicationContext(), PrincipalActivity.class);
+        startActivity(toPrincipaActivity);
+
+    }
+
 }
